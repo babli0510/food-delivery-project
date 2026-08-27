3865216b5b5750df00812af2f12c3e244fbabdfd
shraddha <shraddhagolekar0510@gmail.com>
2026-08-27 10:17:19 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -15,8 +15,7 @@ const API_URL =
   "http://localhost:5000/api";
 
 const api = axios.create({
-  baseURL: API_URL,
-
+baseURL: "https://food-delivery-project-403f.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
